f8532c4d93f7c102d1772a77accf77dd6bb162ba
Rayyanafie <rayyanafie@gmail.com>
2023-03-07 11:54:15 +0700

    Update Biodata

---
+++ 21081010119_Rayya Ruwa'im Nafie_Biodata/Perkenalan.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="PernekalanStyle.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $Nama = "Rayya Ruwa'im Nafie";
+    $Alamat = "Griya Kebraon";
+    $Status = "Undergraduate Student";
+    $Hobby = "Listening Music";
+    $NPM = "21081010119";
+    ?>
+    <div class="grid-container">
+        <div class="item1"><img src="Picture/Formal-removebg-preview.png" height="200" width="200"></div>
+        <div class="Information">
+            <h2>Information</h2>
+                <ul>
+                    <li>NPM:<?php echo $NPM ?></li>
+                    <li>Age:19 Years</li>
+                    <li>Alamat:Griya Kebraon</li>
+                    <li>Status:Student</li>
+                    <li>Hobby:Listening Music</li>
+                </ul>  
+        </div>
+        <div class="Tittle">
+            <b>Rayya Ruwa'im Nafie</b> <br>
+            <b>Undergraduate Student</b>   
+            <div class="Social">
+                <img src="Picture/linkedin.png" alt="" height="20px" width="20px">
+                <p>linkedin.com/in/Rayyanafie</p>
+                <img src="Picture/Gmail.png" alt=""height="20px" width="20px">
+                <p>rayyanafie@gmail.com</p>
+            </div>
+        </div>  
+        <div class="Introduction">
+            <h2>
+            Introduction 
+            </h2>
+        <p>My name is Rayya Ruwa'im Nafie and im an undergraduate student of informatics engineering at UPN "Veteran" Jawatimur. 
+            i'm interested in learning about cybersecurity</p></div>
+        <div class="Skill">
+            <h3>Relevant Skill</h3>
+                    <ul>
+                        <li>C</li>
+                        <li>MySql</li>
+                    </ul>
+        </div>
+        <div class="Education">
+            <h2>Education History</h2>
+            <div class="Education_badge">
+                <img src="Picture/education.png" alt="" height="30px"width = 30px>
+                <b><p> SMAN 15 Surabaya</p></b>
+                <img src="Picture/education.png" alt="" height="30px"width = 30px>  
+                <b><p>Undergraduate Of informatics At UPN "Veteran" Jawa Timur</p></b>
+            </div>
+                    <div >
+                        
+                    </div>
+        </div>
+        <div class="Certification">
+            <h2>Certifications</h2>
+            <div class="Certification_badge">
+                <img src="Picture/badge.png" alt=""height="30px"width = 30px>
+                <b>Fundamental Of Cybersecurity</b> 
+                <p>institution : Course Net 2023</p>
+            </div>           
+        </div>
+</body>
+</html>
